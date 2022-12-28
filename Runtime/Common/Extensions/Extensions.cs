9c9a2a3b1e3d049f0a9f6ffa0ae1eff02f3ea1f4
razorrules <64297142+razorrules@users.noreply.github.com>
2022-12-27 23:42:07 -0500

    updates

--- Runtime/Common/Extensions/Extensions.cs
+++ Runtime/Common/Extensions/Extensions.cs
@@ -224,96 +224,6 @@ public static class Extensions
 
     //----------------- Other -----------------//
 
-    /// <summary>
-    /// Saves the scriptable obejct as a JSON to persistent data path.
-    /// </summary>
-    public static void Save(this ScriptableObject objectToSave, string name, string path = "/Save/", string extension = ".txt")
-    {
-        if (objectToSave == null)
-        {
-            Debug.LogError("Attempted to save a null object");
-            return;
-        }
-        if (!Helper.HasSaveDirectory())
-        {
-            Directory.CreateDirectory(Application.persistentDataPath + path);
-        }
-
-        string jsonSave = JsonUtility.ToJson(objectToSave);
-        using (FileStream file = File.Create(Application.persistentDataPath + path + name + extension))
-        {
-            byte[] bytes = Encoding.ASCII.GetBytes(jsonSave);
-            file.Write(bytes, 0, bytes.Length);
-        }
-
-    }
-
-    /// <summary>
-    /// Load a scriptable object
-    /// </summary>
-    public static ScriptableObject LoadObject<T>(string name, string path = "/Save/", string extension = ".txt")
-    {
-        ScriptableObject returnObj = ScriptableObject.CreateInstance(typeof(T));
-
-        if (!Helper.HasSaveDirectory(path))
-        {
-            Directory.CreateDirectory(Application.persistentDataPath + path);
-            return null;
-        }
-
-        if (File.Exists(Application.persistentDataPath + path + name + extension))
-        {
-            string fileContents;
-
-            FileStream file = File.Open(Application.persistentDataPath + path + name + extension, FileMode.Open);
-            using (StreamReader reader = new StreamReader(file))
-            {
-                fileContents = reader.ReadToEnd();
-            }
-            file.Close();
-
-            JsonUtility.FromJsonOverwrite(fileContents, returnObj);
-
-            return returnObj;
-        }
-
-        Debug.LogWarning("No save detected.");
-        return null;
-    }
-
-    /// <summary>
-    /// Load a scriptable object
-    /// </summary>
-    public static ScriptableObject LoadObject(Type type, string name, string path = "/Save/", string extension = ".txt")
-    {
-        ScriptableObject returnObj = ScriptableObject.CreateInstance(type);
-
-        if (!Helper.HasSaveDirectory(path))
-        {
-            Directory.CreateDirectory(Application.persistentDataPath + path);
-            Debug.Log("Path does not exist");
-            return null;
-        }
-
-        if (File.Exists(Application.persistentDataPath + path + name + extension))
-        {
-            string fileContents;
-
-            FileStream file = File.Open(Application.persistentDataPath + path + name + extension, FileMode.Open);
-            using (StreamReader reader = new StreamReader(file))
-            {
-                fileContents = reader.ReadToEnd();
-            }
-            file.Close();
-
-            JsonUtility.FromJsonOverwrite(fileContents, returnObj);
-
-            return returnObj;
-        }
-        Debug.LogWarning("No save detected.");
-        return null;
-    }
-
     public static List<GameObject> GetAllChildren(this Transform transform)
     {
         List<GameObject> children = new List<GameObject>();
@@ -331,11 +241,4 @@ public static class Extensions
         cam.orthographicSize = desiredHeight * ratio;
     }
 
-    /// <summary>
-    /// Load a scriptable object
-    /// </summary>
-    public static void Load(this ScriptableObject obj, string name, string path = "/Save/", string extension = ".txt")
-    {
-        obj = LoadObject(obj.GetType(), name, path, extension);
-    }
 }
